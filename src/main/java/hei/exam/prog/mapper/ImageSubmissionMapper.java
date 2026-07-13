089807ee9411c4bad266e79faa4209fb438c2d6f
Fiderana-17 <hei.fiderana@gmail.com>
2026-07-13 08:55:24 +0300

    Add ImageSubmissionMapper for converting ImageSubmission entities to responses

---
+++ src/main/java/hei/exam/prog/mapper/ImageSubmissionMapper.java
@@ -0,0 +1,13 @@
+package hei.exam.prog.mapper;
+
+import hei.exam.prog.dto.response.ImageSubmissionResponse;
+import hei.exam.prog.entity.ImageSubmission;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImageSubmissionMapper {
+
+    public ImageSubmissionResponse toResponse(ImageSubmission entity) {
+        return new ImageSubmissionResponse(entity.getId(), entity.getFileName(), entity.getEmail());
+    }
+}
